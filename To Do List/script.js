35fc9abbaa5020a81969d6a058ab1a4a1956c8cc
Shenae.dev <59896817+Shenae-dev@users.noreply.github.com>
2021-07-20 13:44:10 -0500

    Add files via upload

---
+++ To Do List/script.js
@@ -0,0 +1,53 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
+
+// My contribution below
+
+document.querySelector('#color').onclick = function () {
+
+    document.body.style.background = randomColors();
+    document.querySelector("#push").style.background =
+      document.body.style.background;
+    
+}
+
+function randomColors() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
